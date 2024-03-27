a03fa0b0ef2973a2f2a0a8012b2abb7edcbae835
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-28 00:25:39 +0300

    Array example project and First question about reverse order
    In these project I am looking forward to use arrays more efficient and learn the key things about arrays and how I deal in algorithms.

---
+++ Exercises/ArraysExamples/Question1.cs
@@ -0,0 +1,24 @@
+﻿namespace ArraysExamples;
+
+public class Question1
+{
+    public void Question()
+    {
+        int[] arr; // Define an array
+        int n; // Size of array
+        Console.WriteLine("Enter the size of array");
+        n = int.Parse(Console.ReadLine()); // Read size of array
+        arr = new int[n]; // Create an array of size n
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Values:");
+            arr[i] = int.Parse(Console.ReadLine()); // Giving values to array
+        }
+
+        for (int i = n-1; i >= 0; i--)// Printing array in reverse order
+        {
+            Console.Write($" {arr[i]} ");
+        }
+    }
+}
