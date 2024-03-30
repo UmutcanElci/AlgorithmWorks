c6bc7ae0cc4c315b5220add75f40391f981726ad
Umutcan Elci <umutcanelci.me@gmail.com>
2024-03-31 01:15:49 +0300

    Stack question 1
    Write a swithc case program to learn the basis

---
+++ Exercises/StackExamples/Question1.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+
+namespace StackExamples;
+
+public class Question1
+{
+    // Write program that implement a stack with push and pop operations.Fidn the top element of the stack and check if it is empty.
+    public void Question()
+    {
+        Stack stack = new Stack();
+
+        Console.WriteLine("Which operation : 1-push 2-pop 3-top 4-empty or anything you want to exit");
+        
+        
+        
+            int input = int.Parse(Console.ReadLine()!);// Not implementing any try catch for different inputs but it works fine for now.
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Value:");
+                    stack.Push(int.Parse(Console.ReadLine()!)); // Add a new element to stack 
+                    break;
+                case 2:
+                    stack.Pop(); // Remove the top element
+                    break;
+                case 3:
+                    stack.Peek(); // Find the top element
+                    break;
+                case 4:
+                    if (stack.Count == 0) // Checking if stack is empty
+                    {
+                        Console.WriteLine("Stack is empty");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Bye");
+                    break;
+            }
+        
+       
+            /*  +---+        
+                | A |
+                +---+  //Stack values
+                | B |
+                +---+
+                | C |
+                +---+
+                
+                +---+
+                | B |
+                +---+
+                | A | // Adding the B again
+                +---+
+                | C |
+                +---+
+
+                +---+
+                | A |
+                +---+ // Pop the value
+                | C |
+                +---+
+
+                */
+
+        
+        
+        
+    }
+}
--- Exercises/StackExamples/obj/Debug/net8.0/StackExamples.AssemblyInfo.cs
+++ Exercises/StackExamples/obj/Debug/net8.0/StackExamples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StackExamples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecce8cf6edf6bb67c3acebe613e1207c81606cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01cbc4cddaa9e712f5a790ca22327108f463b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("StackExamples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StackExamples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
