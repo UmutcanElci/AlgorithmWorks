79e2eb6c904e1c611a9f0a9c8af235c85eea9866
Umutcan Elçi <umutcanelci@yaani.com>
2024-04-04 09:47:48 +0300

    Draw a 3x3 array
    Learning to create 2d array and print it

---
+++ Exercises/ArraysExamples/Question6.cs
@@ -0,0 +1,21 @@
+﻿namespace ArraysExamples;
+
+public class Question6
+{
+    // Wrıte a 2D array and print it 3x3
+    public void Question()
+    {
+        int[][] arr = new int[3][]; // 3 rows
+        arr[0] = new int[3] { 1, 2, 3 };
+        arr[1] = new int[3] { 4, 5, 6 };
+        arr[2] = new int[3] { 7, 8, 9 };
+        foreach (int[] row in arr)
+        {
+            foreach (int element in row)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
