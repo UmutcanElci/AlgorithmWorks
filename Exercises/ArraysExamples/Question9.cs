b611b76d231d7481e6773484131780b563bc3f07
Umutcan Elci <umutcanelci.me@gmail.com>
2024-04-07 19:31:42 +0300

    Find the odd numbers of the array
    Checking every array values for the odd values

---
+++ Exercises/ArraysExamples/Question9.cs
@@ -0,0 +1,17 @@
+﻿namespace ArraysExamples;
+
+public class Question9
+{//Get only odd values from a give integer array
+    public void Question()
+    {
+        int[] arr = { 1, 4, 7, 23, 5, 2, 12, 56, 33, 7, 22, 106, 71 };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+               Console.Write(arr[i] + "  "); 
+            }
+        }
+    }
+}
