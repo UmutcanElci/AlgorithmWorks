7415f463655823daf4074d8297ca58b357bc2225
Umutcan Elçi <umutcanelci@yaani.com>
2024-04-09 02:56:51 +0300

    Adding bubblesort
    Bubble sort is one of the easiest algoritms to make is like a Selection sort(but slower,and maybe bit more accurate)

---
+++ Exercises/SortingAlgoritms/BubbleSort.cs
@@ -0,0 +1,20 @@
+﻿namespace SortingAlgoritms;
+
+public class BubbleSort
+{// O(n^2) 
+    public static void Sort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+            {
+                if (array[j] > array[j+1])//If left is greater than right
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;//Swap
+                }
+            }
+        }
+    }
+}
