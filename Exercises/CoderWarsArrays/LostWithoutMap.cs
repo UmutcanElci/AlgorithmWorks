fd11748004fc5ab08922e780b9cc0e47f1bac9b3
Umutcan Elçi <umutcanelci@yaani.com>
2024-04-15 17:38:06 +0300

    Mult by 2 the values of array

---
+++ Exercises/CoderWarsArrays/LostWithoutMap.cs
@@ -0,0 +1,15 @@
+
+namespace CoderWarsArrays;
+
+public class LostWithoutMap
+{
+    public int[] Maps (int[] x)
+    {
+
+	    for(int i = 0; i < x.Length; i++)
+	    {
+		    x[i] = x[i] * 2;
+	    }
+	    return x;
+    }
+
